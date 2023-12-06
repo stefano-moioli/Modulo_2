27df3db04b7104a704392dd47009823642b51915
stefano-moioli <stefano.moioli10@gmail.com>
2023-12-06 18:48:10 +0100

    javascriptSetup

---
+++ Progetto1/js/script.js
@@ -0,0 +1,6 @@
+const nuovoDiv = document.getElementById('nuovoDiv');
+const testoDentroAlDiv = document.createElement('p');
+
+testoDentroAlDiv.innerText = 'Testo dentro al div';
+nuovoDiv.append(testoDentroAlDiv);
+
