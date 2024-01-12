a87bffb58ea0c7d9d377093956b5b92a3a9ee726
stefano-moioli <stefano.moioli10@gmail.com>
2024-01-12 12:17:21 +0100

    writtenSearchFunction

--- W4D1_SearchFunction/js/script.js
+++ W4D1_SearchFunction/js/script.js
@@ -4,6 +4,19 @@ const inputLocation = document.querySelector("#inputLocation");
 
 const results = [];
 
-function searchJobAndLocation(){
-
+function searchJobAndLocation(title, location){
+    title = title.toLowerCase();
+    location = location.toLowerCase();
+    
+    jobs.forEach(job => {
+    const jobTitle = job.title.toLocaleLowerCase();
+    const jobLocation = job.location.toLocaleLowerCase();
+    if(jobTitle.includes(title) && jobLocation.includes(location)){
+    results.push(job);
+    }
+    });
+    return {
+        count: results.lenght,
+        results
+    }
 }
