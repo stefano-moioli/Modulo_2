30f8d074473c0ffa84d2f25b0e89e4b059534c14
stefano-moioli <stefano.moioli10@gmail.com>
2024-01-12 11:58:16 +0100

    searchFunctionSetup

---
+++ W4D1_SearchFunction/js/script.js
@@ -0,0 +1,9 @@
+const searchButton = document.querySelector("#searchButton");
+const inputJob = document.querySelector("#inputJob");
+const inputLocation = document.querySelector("#inputLocation");
+
+const results = [];
+
+function searchJobAndLocation(){
+
+}
